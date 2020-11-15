72fd3c4beecabce6ded9ce5ac41d0cfe1d0ad1b4
Valérie Thisdale <radio0but@gmail.com>
2020-11-15 00:25:52 -0500

    added lockdown template in install script

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ cd ./LXD-Control-Center
 random=$(tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c50)
 python3 -m venv venv
 source venv/bin/activate
+mv base.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
+mv form.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
 pip install psutil
 pip install django
 pip install requests

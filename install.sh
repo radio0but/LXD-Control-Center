ffa129b35c5fbbf551200a405db7df5b3f7e8e2d
Valérie Thisdale <radio0but@gmail.com>
2020-11-14 23:00:48 -0500

    Fixed install scipt

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 apt install python3 git
+cd /opt
 git clone https://github.com/radio0but/LXD-Control-Center.git
+cd ./LXD-Control-Center
 random=$(tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c50)
 python3 -m venv venv
 source venv/bin/activate
 pip install psutil
 pip install django
 pip install requests
-random="'"+ $random + "'"
-awk -v secretKey="$random" '{print} /SECRET_KEY =/{print secretKey}' ./LXD-Control-Center/LXDcontrolCENTER/settings.py
+echo "SECRET_KEY = '$random'" >> ./LXDcontrolCENTER/settings.py

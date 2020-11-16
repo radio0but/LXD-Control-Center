e260c2d5f7dac90e13c8471e12a82803d6f43790
Valérie Thisdale <radio0but@gmail.com>
2020-11-15 19:47:48 -0500

    Modified install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
-ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
-VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
+# OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+# ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+# VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
 
 # if [ -z "$OS" ]; then
 #     OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')

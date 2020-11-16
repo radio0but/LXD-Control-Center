2ad743b2b03de0a826aa98a68b33a522333b53aa
radio0but <radio0but@gmail.com>
2020-11-15 22:27:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-
-cd /opt
-git clone https://github.com/radio0but/LXD-Control-Center.git
-cd ./LXD-Control-Center
-random=$(tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c50)
-python3 -m venv venv
-source venv/bin/activate
-pip install psutil
-pip install django
-pip install requests
-pip install django-lockdown
-rm venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/base.html
-rm venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/form.html
-mv base.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
-mv form.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
-echo "SECRET_KEY = '$random'" >> ./LXDcontrolCENTER/settings.py
-python manage.py migrate
-python manage.py createsuperuser
+####################################################################################
+###                                                                              ###
+###                    Installer for LXD-Control-Center                          ###
+###                                                                              ###
+####################################################################################
+####################################################################################
+## change directory to /opt                                                       ##
+cd /opt                                                                           ##
+## Clone the files from github                                                    ##
+git clone https://github.com/radio0but/LXD-Control-Center.git                     ##
+## Change directory to LXD-Control-Center/                                        ##
+cd ./LXD-Control-Center                                                           ##
+## Create a random number for encryption                                          ##
+random=$(tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c50)                ##
+## Create a Virtual environement for Python dependensies                          ##
+python3 -m venv venv                                                              ##
+## Enter in the virtual environement                                              ##
+source venv/bin/activate                                                          ##
+## Install Dependensies                                                           ##
+pip install psutil                                                                ##
+pip install django                                                                ##
+pip install requests                                                              ##
+pip install django-lockdown                                                       ##
+## Remove the default templates for Django Lockdown                               ##
+rm venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/base.html       ##
+rm venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/form.html       ##
+## Move the custom templates for Django Lockdown                                  ##  
+mv base.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/      ##    
+mv form.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/      ##
+## Add the random string to Lxd Control Center setting file                       ##
+echo "SECRET_KEY = '$random'" >> ./LXDcontrolCENTER/settings.py                   ##
+## create the sqlite database                                                     ##
+python manage.py migrate                                                          ##
+## Create root User for Lxd Control Center                                        ##            
+python manage.py createsuperuser                                                  ##
+####################################################################################

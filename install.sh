c6631abf532c54e831657039b40f41d2e3f82e44
Valérie Thisdale <radio0but@gmail.com>
2020-11-15 19:53:08 -0500

    Modified install.sh

--- install.sh
+++ install.sh
@@ -24,12 +24,12 @@ cd ./LXD-Control-Center
 random=$(tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c50)
 python3 -m venv venv
 source venv/bin/activate
-pip install django-lockdown
-mv base.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
-mv form.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
 pip install psutil
 pip install django
 pip install requests
+pip install django-lockdown
+mv base.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
+mv form.html venv/lib64/python3.8/site-packages/lockdown/templates/lockdown/
 echo "SECRET_KEY = '$random'" >> ./LXDcontrolCENTER/settings.py
 python manage.py migrate
 python manage.py createsuperuser
